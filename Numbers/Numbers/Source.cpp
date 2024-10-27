afede8685623ee2e55422fa079069106ed4efda8
Milosz Kropielnicki <c00309394@setu.ie>
2024-10-27 10:52:02 +0000

    Odd numbers

--- Numbers/Numbers/Source.cpp
+++ Numbers/Numbers/Source.cpp
@@ -1,9 +1,18 @@
 //Odd Milosz
+//Even Milosz
 
 #include <iostream> 
 
 int main()
 {
+	int input = 0;
+	std::cout << "Input a number of your choice: ";
+	std::cin >> input;
+
+	if (input % 2 == 1)
+	{
+		std::cout << "The number is odd" << std::endl;
+	}
 
 	return 1;
 }
