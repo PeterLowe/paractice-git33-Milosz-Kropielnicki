60dc04321d920fad4d9b4e89b294e2c560302f80
Milosz Kropielnicki <c00309394@setu.ie>
2024-10-27 10:58:41 +0000

    Even numbers

--- Numbers/Numbers/Source.cpp
+++ Numbers/Numbers/Source.cpp
@@ -1,4 +1,3 @@
-//Odd Milosz
 //Even Milosz
 
 #include <iostream> 
@@ -6,12 +5,13 @@
 int main()
 {
 	int input = 0;
-	std::cout << "Input a number of your choice: ";
+
+	std::cout << "Please input a number: ";
 	std::cin >> input;
 
-	if (input % 2 == 1)
+	if (input % 2 == 0)
 	{
-		std::cout << "The number is odd" << std::endl;
+		std::cout << "The number is even." << std::endl;
 	}
 
 	return 1;
